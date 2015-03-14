aa6bf948c686c6b7c54429d8e93bfda1e276f9d4
Manuel Naujoks <mnaujoks@gmail.com>
2015-03-14 13:29:07 +0100

    appveyor automatic nuget publish enabled

---
+++ .nuget/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\Result.OrDefault\bin\Release\Result.OrDefault.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\Result.OrDefault.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\Result.OrDefault.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\nuget\Result.OrDefault.compiled.nuspec
