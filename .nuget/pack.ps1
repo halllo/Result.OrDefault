1efea66b861e2ce94c4234bdd6f77163f13d7879
Manuel Naujoks <mnaujoks@gmail.com>
2015-03-14 13:41:46 +0100

    pack.ps1 path corrected

--- .nuget/pack.ps1
+++ .nuget/pack.ps1
@@ -4,9 +4,9 @@ $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\NuGet\Result.OrDefault.nuspec) 
+$content = (Get-Content $root\.nuget\Result.OrDefault.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\nuget\Result.OrDefault.compiled.nuspec
+$content | Out-File $root\.nuget\Result.OrDefault.compiled.nuspec
 
-& $root\NuGet\NuGet.exe pack $root\nuget\Result.OrDefault.compiled.nuspec
+& $root\NuGet\NuGet.exe pack $root\.nuget\Result.OrDefault.compiled.nuspec
